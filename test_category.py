3d46bc9e6eed6a6dab85bac6be766b52f19ad020
pedrojpx <pjpxavier@gmail.com>
2025-03-24 20:17:28 -0300

    switched unittest for pytest

--- test_category.py
+++ test_category.py
@@ -1,38 +1,34 @@
-import unittest
+import pytest
 
 from category import Category
 from uuid import UUID
 
-class TestCategory(unittest.TestCase):
+class TestCategory():
     def test_name_is_required(self):
-        with self.assertRaisesRegex(TypeError, "missing 1 required positional argument: 'name'"):
+        with pytest.raises(TypeError, match="missing 1 required positional argument: 'name'"):
             Category()
 
     def test_name_must_have_less_than_255_characters(self):
-        with self.assertRaisesRegex(TypeError, "name must have less than 256 characters"):
+        with pytest.raises(TypeError, match="name must have less than 256 characters"):
             Category(name="a"*256)
 
     def test_category_must_be_created_with_id_as_uuid(self):
         c = Category(name="filme")
-        self.assertEqual(type(c.id), UUID)
+        assert isinstance(c.id, UUID)
 
     def test_category_active_by_default(self):
         c = Category(name="filme")
-        self.assertEqual(c.is_active, True)
+        assert c.is_active == True
 
     def test_created_category_with_default_values(self):
         c = Category(name="filme")
-        self.assertEqual(c.name, "filme")
-        self.assertEqual(c.description, "")
-        self.assertEqual(c.is_active, True)
+        assert c.name == "filme"
+        assert c.description == ""
+        assert c.is_active is True
 
     def test_created_category_with_provided_values(self):
         c = Category("filme", "id", "description", False)
-        self.assertEqual(c.id, "id")
-        self.assertEqual(c.name, "filme")
-        self.assertEqual(c.description, "description")
-        self.assertEqual(c.is_active, False)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert c.id == "id"
+        assert c.name == "filme"
+        assert c.description == "description"
+        assert c.is_active is False
