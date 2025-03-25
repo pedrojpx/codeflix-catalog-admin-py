4567f6bc047572df08e34d67bfc2c5cef2ea2902
pedrojpx <pjpxavier@gmail.com>
2025-03-25 16:11:37 -0300

    implemented in memory repo to usecase and tests

--- src/core/category/application/create_category.py
+++ src/core/category/application/create_category.py
@@ -1,13 +1,19 @@
 from uuid import UUID
 
+from core.category.infra.in_memory_category import InMemoryCategoryRepository
 from src.core.category.domain.category import Category
 
-def create_category(name: str, description: str = "", is_active: bool = True) -> UUID:
+def create_category(
+        repository: InMemoryCategoryRepository,
+        name: str, 
+        description: str = "", 
+        is_active: bool = True) -> UUID:
     try:
         c = Category(name=name, description=description, is_active=is_active)
     except ValueError as err:
         raise InvalidCategoryData(err)
     
+    repository.save(c)
     return c.id
 
 class InvalidCategoryData(Exception):
--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -1,23 +1,28 @@
+from unittest.mock import MagicMock
 from uuid import UUID
 
 import pytest
 
+from src.core.category.infra.in_memory_category import InMemoryCategoryRepository
 from src.core.category.application.create_category import create_category, InvalidCategoryData
 
 class TestCreateCategory:
     def test_create_w_valid_data(self):
-        c_id = create_category(name="Filme", description="Categoria para filmes", is_active=True)
+        mock_repo = MagicMock(InMemoryCategoryRepository)
+        c_id = create_category(repository=mock_repo, name="Filme", description="Categoria para filmes", is_active=True)
 
         assert c_id is not None
         assert isinstance(c_id, UUID)
+        assert mock_repo.save.called
     
     def test_create_w_invalid_data(self):
+        mock_repo = MagicMock(InMemoryCategoryRepository)
         with pytest.raises(InvalidCategoryData, match="name cannot be empty") as exception_info:
-            c_id = create_category(name="")
+            c_id = create_category(repository=mock_repo, name="")
 
         assert exception_info.type is InvalidCategoryData
         
         with pytest.raises(InvalidCategoryData, match="name cannot be longer than 255 characters") as exception_info:
-            c_id = create_category(name="a"*256)
+            c_id = create_category(repository=mock_repo, name="a"*256)
 
         assert exception_info.type is InvalidCategoryData
